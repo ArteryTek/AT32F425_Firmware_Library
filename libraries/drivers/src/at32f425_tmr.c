8474926c94c75378e836727e6e9e706cc0e7924b
Artery-MCU <tristamao@arterytek.com>
2023-10-30 11:28:09 +0800

    update version to 2.0.9

--- libraries/drivers/src/at32f425_tmr.c
+++ libraries/drivers/src/at32f425_tmr.c
@@ -1278,10 +1278,45 @@ void tmr_interrupt_enable(tmr_type *tmr_x, uint32_t tmr_interrupt, confirm_state
 }
 
 /**
+  * @brief  get tmr interrupt flag
+  * @param  tmr_x: select the tmr peripheral.
+  *         this parameter can be one of the following values:
+  *         TMR1, TMR2, TMR3, TMR6, TMR7, TMR13, TMR14, TMR15,
+  *         TMR16, TMR17
+  * @param  tmr_flag
+  *         this parameter can be one of the following values:
+  *         - TMR_OVF_FLAG
+  *         - TMR_C1_FLAG
+  *         - TMR_C2_FLAG
+  *         - TMR_C3_FLAG
+  *         - TMR_C4_FLAG
+  *         - TMR_HALL_FLAG
+  *         - TMR_TRIGGER_FLAG
+  *         - TMR_BRK_FLAG
+  * @retval state of tmr interrupt flag
+  */
+flag_status tmr_interrupt_flag_get(tmr_type *tmr_x, uint32_t tmr_flag)
+{
+  flag_status status = RESET;
+
+  if((tmr_x->ists & tmr_flag) && (tmr_x->iden & tmr_flag))
+  {
+    status = SET;
+  }
+  else
+  {
+    status = RESET;
+  }
+
+  return status;
+}
+
+/**
   * @brief  get tmr flag
   * @param  tmr_x: select the tmr peripheral.
   *         this parameter can be one of the following values:
-  *         TMR1, TMR2, TMR3, TMR6, TMR7, TMR13, TMR14, TMR15, TMR16, TMR17
+  *         TMR1, TMR2, TMR3, TMR6, TMR7, TMR13, TMR14, TMR15,
+  *         TMR16, TMR17
   * @param  tmr_flag
   *         this parameter can be one of the following values:
   *         - TMR_OVF_FLAG
@@ -1317,7 +1352,9 @@ flag_status tmr_flag_get(tmr_type *tmr_x, uint32_t tmr_flag)
 /**
   * @brief  clear tmr flag
   * @param  tmr_x: select the tmr peripheral.
-  *         TMR1, TMR2, TMR3, TMR6, TMR7, TMR13, TMR14, TMR15, TMR16, TMR17
+  *         this parameter can be one of the following values:
+  *         TMR1, TMR2, TMR3, TMR6, TMR7, TMR13, TMR14, TMR15,
+  *         TMR16, TMR17
   * @param  tmr_flag
   *         this parameter can be any combination of the following values:
   *         - TMR_OVF_FLAG
@@ -1343,7 +1380,8 @@ void tmr_flag_clear(tmr_type *tmr_x, uint32_t tmr_flag)
   * @brief  generate tmr event
   * @param  tmr_x: select the tmr peripheral.
   *         this parameter can be one of the following values:
-  *         TMR1, TMR2, TMR3, TMR6, TMR7, TMR13, TMR14, TMR15, TMR16, TMR17
+  *         TMR1, TMR2, TMR3, TMR6, TMR7, TMR13, TMR14, TMR15,
+  *         TMR16, TMR17
   * @param  tmr_event
   *         this parameter can be one of the following values:
   *         - TMR_OVERFLOW_SWTRIG
